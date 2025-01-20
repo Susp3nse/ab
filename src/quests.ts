787569074bf48aafa4b84e4fd401a693275de426
Bryan <Bryan.cordes@oasisdigital.com>
2025-01-20 15:48:51 -0500

    deploy

---
+++ src/quests.ts
@@ -0,0 +1,344 @@
+export const OSRSQuests = {
+  animalmag: {
+    fullName: "Animal Magnetism",
+    builderName: "animalmag",
+  },
+  barcrawl: {
+    fullName: "Bar Crawl",
+    builderName: "barcrawl",
+  },
+  belowicemountain: {
+    fullName: "Below Ice Mountain",
+    builderName: "belowicemountain",
+  },
+  bigchompybirdhunting: {
+    fullName: "Big Chompy Bird Hunting",
+    builderName: "bigchompybirdhunting",
+  },
+  biohazard: {
+    fullName: "Biohazard",
+    builderName: "biohazard",
+  },
+  blackknightfortress: {
+    fullName: "Black Knight's Fortress",
+    builderName: "blackknightfortress",
+  },
+  bonevoyage: {
+    fullName: "Bone Voyage",
+    builderName: "bonevoyage",
+  },
+  clientkourend: {
+    fullName: "Client of Kourend",
+    builderName: "clientkourend",
+  },
+  cooks: {
+    fullName: "Cook's Assistant",
+    builderName: "cooks",
+  },
+  creaturefenkenstrain: {
+    fullName: "Creature of Fenkenstrain",
+    builderName: "creaturefenkenstrain",
+  },
+  deathplateau: {
+    fullName: "Death Plateau",
+    builderName: "deathplateau",
+  },
+  demonslayer: {
+    fullName: "Demon Slayer",
+    builderName: "demonslayer",
+  },
+  depthsdespair: {
+    fullName: "Depths of Despair",
+    builderName: "depthsdespair",
+  },
+  digsite: {
+    fullName: "The Dig Site",
+    builderName: "digsite",
+  },
+  dorics: {
+    fullName: "Dorics Quest",
+    builderName: "dorics",
+  },
+  druidicritual: {
+    fullName: "Druidic Ritual",
+    builderName: "druidicritual",
+  },
+  dwarfcanon: {
+    fullName: "Dwarf Cannon",
+    builderName: "dwarfcanon",
+  },
+  elemworkshopone: {
+    fullName: "Elemental Workshop I",
+    builderName: "elemworkshopone",
+  },
+  enterabyss: {
+    fullName: "Enter the Abyss",
+    builderName: "enterabyss",
+  },
+  ernestchicken: {
+    fullName: "Ernest the Chicken",
+    builderName: "ernestchicken",
+  },
+  fightarena: {
+    fullName: "Fight Arena",
+    builderName: "fightarena",
+  },
+  fishcontest: {
+    fullName: "Fishing Contest",
+    builderName: "fishcontest",
+  },
+  fremmytrials: {
+    fullName: "Fremennik Trials",
+    builderName: "fremmytrials",
+  },
+  gertrudes: {
+    fullName: "Gertrude's Cat",
+    builderName: "gertrudes",
+  },
+  ghostsahoy: {
+    fullName: "Ghosts Ahoy",
+    builderName: "ghostsahoy",
+  },
+  goblindip: {
+    fullName: "Goblin Diplomacy",
+    builderName: "goblindip",
+  },
+  grandtree: {
+    fullName: "The Grand Tree",
+    builderName: "grandtree",
+  },
+  hazeelcult: {
+    fullName: "Hazeel Cult",
+    builderName: "hazeelcult",
+  },
+  holygrail: {
+    fullName: "The Holy Grail",
+    builderName: "holygrail",
+  },
+  horror: {
+    fullName: "Horror from the Deep",
+    builderName: "horror",
+  },
+  impcatcher: {
+    fullName: "Imp Catcher",
+    builderName: "impcatcher",
+  },
+  itchyhelper: {
+    fullName: "The Itchy Helper",
+    builderName: "itchyhelper",
+  },
+  junglepotion: {
+    fullName: "Jungle Potion",
+    builderName: "junglepotion",
+  },
+  knightsword: {
+    fullName: "The Knight's Sword",
+    builderName: "knightsword",
+  },
+  lostcity: {
+    fullName: "Lost City",
+    builderName: "lostcity",
+  },
+  losttribe: {
+    fullName: "The Lost Tribe",
+    builderName: "losttribe",
+  },
+  makinghistory: {
+    fullName: "Making History",
+    builderName: "makinghistory",
+  },
+  merlincrystal: {
+    fullName: "Merlin's Crystal",
+    builderName: "merlincrystal",
+  },
+  monkeymadness: {
+    fullName: "Monkey Madness I",
+    builderName: "monkeymadness",
+  },
+  monkeymadnessdareo: {
+    fullName: "Monkey Madness II",
+    builderName: "monkeymadnessdareo",
+  },
+  monksfriend: {
+    fullName: "Monk's Friend",
+    builderName: "monksfriend",
+  },
+  mountaindaughter: {
+    fullName: "Mountain Daughter",
+    builderName: "mountaindaughter",
+  },
+  murdermystery: {
+    fullName: "Murder Mystery",
+    builderName: "murdermystery",
+  },
+  naturespirit: {
+    fullName: "Nature Spirit",
+    builderName: "naturespirit",
+  },
+  observatory: {
+    fullName: "The Observatory Quest",
+    builderName: "observatory",
+  },
+  onesmallfavour: {
+    fullName: "One Small Favour",
+    builderName: "onesmallfavour",
+  },
+  piratestreasure: {
+    fullName: "Pirate's Treasure",
+    builderName: "piratestreasure",
+  },
+  plaguecity: {
+    fullName: "Plague City",
+    builderName: "plaguecity",
+  },
+  porcineofinterest: {
+    fullName: "Porcine of Interest",
+    builderName: "porcineofinterest",
+  },
+  priestperil: {
+    fullName: "Priest in Peril",
+    builderName: "priestperil",
+  },
+  princealirescue: {
+    fullName: "Prince Ali Rescue",
+    builderName: "princealirescue",
+  },
+  queenthieves: {
+    fullName: "The Queen of Thieves",
+    builderName: "queenthieves",
+  },
+  recruitmentdrive: {
+    fullName: "Recruitment Drive",
+    builderName: "recruitmentdrive",
+  },
+  restlessghost: {
+    fullName: "The Restless Ghost",
+    builderName: "restlessghost",
+  },
+  rfdgoblin: {
+    fullName: "Recipe for Disaster - Goblin subquest",
+    builderName: "rfdgoblin",
+  },
+  rfdstart: {
+    fullName: "Recipe for Disaster - Start",
+    builderName: "rfdstart",
+  },
+  romeojuliet: {
+    fullName: "Romeo & Juliet",
+    builderName: "romeojuliet",
+  },
+  runemysteries: {
+    fullName: "Rune Mysteries",
+    builderName: "runemysteries",
+  },
+  scorpioncatcher: {
+    fullName: "Scorpion Catcher",
+    builderName: "scorpioncatcher",
+  },
+  searchofmyreque: {
+    fullName: "Search for My Reque",
+    builderName: "searchofmyreque",
+  },
+  seaslug: {
+    fullName: "Sea Slug",
+    builderName: "seaslug",
+  },
+  shadowstorm: {
+    fullName: "Shadow of the Storm",
+    builderName: "shadowstorm",
+  },
+  sheepshearer: {
+    fullName: "Sheep Shearer",
+    builderName: "sheepshearer",
+  },
+  shilovillage: {
+    fullName: "Shilo Village",
+    builderName: "shilovillage",
+  },
+  sleepinggiants: {
+    fullName: "Sleeping Giants",
+    builderName: "sleepinggiants",
+  },
+  taibaitrio: {
+    fullName: "Tai Bait Riots",
+    builderName: "taibaitrio",
+  },
+  tailoftwocats: {
+    fullName: "A Tail of Two Cats",
+    builderName: "tailoftwocats",
+  },
+  templeeye: {
+    fullName: "Temple of Eye",
+    builderName: "templeeye",
+  },
+  thefeud: {
+    fullName: "The Feud",
+    builderName: "thefeud",
+  },
+  thegolem: {
+    fullName: "The Golem",
+    builderName: "thegolem",
+  },
+  touristtrap: {
+    fullName: "Tourist Trap",
+    builderName: "touristtrap",
+  },
+  treegnomevillage: {
+    fullName: "Tree Gnome Village",
+    builderName: "treegnomevillage",
+  },
+  tribaltotem: {
+    fullName: "Tribal Totem",
+    builderName: "tribaltotem",
+  },
+  trollromance: {
+    fullName: "Troll Romance",
+    builderName: "trollromance",
+  },
+  trollstronghold: {
+    fullName: "Troll Stronghold",
+    builderName: "trollstronghold",
+  },
+  ugpass: {
+    fullName: "Underground Pass",
+    builderName: "ugpass",
+  },
+  vampireslayer: {
+    fullName: "Vampire Slayer",
+    builderName: "vampireslayer",
+  },
+  varrockmuseum: {
+    fullName: "Varrock Museum",
+    builderName: "varrockmuseum",
+  },
+  wanted: {
+    fullName: "Wanted!",
+    builderName: "wanted",
+  },
+  waterfall: {
+    fullName: "Waterfall Quest",
+    builderName: "waterfall",
+  },
+  witchshouse: {
+    fullName: "The Witch's House",
+    builderName: "witchshouse",
+  },
+  witchspotion: {
+    fullName: "Witch's Potion",
+    builderName: "witchspotion",
+  },
+  xmarks: {
+    fullName: "X Marks the Spot",
+    builderName: "xmarks",
+  },
+  zogreflesheaters: {
+    fullName: "Zogre Flesh Eaters",
+    builderName: "zogreflesheaters",
+  },
+};
+
+export const elfQuests = {};
+export const gnomeQuests = {};
+export const basicQuests = {};
+
+export default OSRSQuests;
